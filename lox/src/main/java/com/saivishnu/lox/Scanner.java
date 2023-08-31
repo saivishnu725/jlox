99936bb45aee48eb1ce0727ab3e087319b53de27
Sai Vishnu <saivishnu725@gmail.com>
2023-08-31 21:59:33 +0530

    wrote the scanToken

--- lox/src/main/java/com/saivishnu/lox/Scanner.java
+++ lox/src/main/java/com/saivishnu/lox/Scanner.java
@@ -1,3 +1,4 @@
+// TODO: comments everywhere
 package com.saivishnu.lox;
 
 import java.util.ArrayList;
@@ -7,8 +8,11 @@ import java.util.Map;
 
 import static com.saivishnu.lox.TokenType.*;
 
-
 public class Scanner {
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
@@ -16,5 +20,67 @@ public class Scanner {
         this.source = source;
     }
 
-    
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+        }
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
 }
