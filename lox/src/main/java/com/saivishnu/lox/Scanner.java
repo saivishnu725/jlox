d34c6a0266c28e7f720e652cc790a7f4ef58da83
Sai Vishnu <saivishnu725@gmail.com>
2023-08-31 21:31:29 +0530

    base Scanner class

---
+++ lox/src/main/java/com/saivishnu/lox/Scanner.java
@@ -0,0 +1,20 @@
+package com.saivishnu.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.saivishnu.lox.TokenType.*;
+
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    
+}
