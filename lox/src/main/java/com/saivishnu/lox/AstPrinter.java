28a4a5b188f3956b893471550602ccbff5e7e8f3
Sai Vishnu <saivishnu725@gmail.com>
2023-09-18 12:08:01 +0530

    a pretty printer

---
+++ lox/src/main/java/com/saivishnu/lox/AstPrinter.java
@@ -0,0 +1,38 @@
+package com.saivishnu.lox;
+
+public class AstPrinter implements Expr.Visitor<String> {
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(name);
+        for (Expr expr : exprs) {
+            sb.append(" ");
+            sb.append(expr.accept(this));
+        }
+        sb.append(")");
+
+        return sb.toString();
+    }
+}
