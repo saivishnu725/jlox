811d946c0db3a055e573986b74e1c333648c80d9
Sai Vishnu <saivishnu725@gmail.com>
2023-09-24 14:21:28 +0530

    factor(), unary(), primary()

--- lox/src/main/java/com/saivishnu/lox/Parser.java
+++ lox/src/main/java/com/saivishnu/lox/Parser.java
@@ -33,40 +33,34 @@ public class Parser {
      */
 
     /*
+     * ********** Methods that check the equation follow these steps **************
      * first assume that it isn't the current condition and then check for it,
      * if it is a match then make it the operator,
      * then assume the right side of it is the next precedence.
      */
 
     private Expr expression() {
-        // 1. check for equality
         return equality();
     }
 
     private Expr equality() {
         // !=, ==
-        // just assume that it isn't equal
         Expr expr = comparison();
 
-        // check for equality
         while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-            Token opeator = previous();
-            // if it is equal then assume the rest is comparison and call it.
+            Token operator = previous();
             Expr right = comparison();
-            expr = new Expr.Binary(expr, opeator, right);
+            expr = new Expr.Binary(expr, operator, right);
         }
         return expr;
     }
 
     private Expr comparison() {
         // <, <=, >, >=
-        // assume that it is isn't comparison
         Expr expr = term();
 
-        // check for comparison
         while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
             Token operator = previous();
-            // next in precedence is +, -
             Expr right = term();
             expr = new Expr.Binary(expr, operator, right);
         }
@@ -74,6 +68,7 @@ public class Parser {
     }
 
     private Expr term() {
+        // -, +
         Expr expr = factor();
 
         while (match(MINUS, PLUS)) {
@@ -85,6 +80,47 @@ public class Parser {
         return expr;
     }
 
+    private Expr factor() {
+        // /, *
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        // !, -
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            // return directly as it is either Unary or primary values
+            return new Expr.Unary(operator, right);
+        }
+        // if not unary, it is primary
+        return primary();
+    }
+
+    private Expr primary() {
+        // NUMBER, STRING, "true", "false", "nil", "(",  expression, ")"
+        if (match(FALSE)) return new Expr.Literal(false);
+        if(match(TRUE)) return new Expr.Literal(true);
+        if(match(NIL)) return new Expr.Literal(null);
+
+        if(match(NUMBER, STRING)) return new Expr.Literal(previous().literal);
+
+        if(match(LEFT_PAREN) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+    }
+
+    // used to check if the operator is same as expected
     private boolean match(TokenType... types) {
         for (TokenType type : types) {
             if (check(type)) {
@@ -95,26 +131,31 @@ public class Parser {
         return false;
     }
 
+    // gets current and checks with expected one
     private boolean check(TokenType type) {
         if (isAtEnd())
             return false;
         return peek().type == type;
     }
 
+    // go to next one
     private Token advance() {
         if (!isAtEnd())
             current++;
         return previous();
     }
 
+    // check end of token stream
     private boolean isAtEnd() {
         return peek().type == EOF;
     }
 
+    // get the current token without going to next one
     private Token peek() {
         return tokens.get(current);
     }
 
+    // get previous token without going to previous one
     private Token previous() {
         return tokens.get(current - 1);
     }
