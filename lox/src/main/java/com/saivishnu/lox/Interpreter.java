dd89dcacf7011aecab4fa245abef958401a6b843
Sai Vishnu <saivishnu725@gmail.com>
2023-09-28 15:34:49 +0530

    literal and grouping visitors

--- lox/src/main/java/com/saivishnu/lox/Interpreter.java
+++ lox/src/main/java/com/saivishnu/lox/Interpreter.java
@@ -2,4 +2,22 @@ package com.saivishnu.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        // return the value which is already stored
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        // since the inside of a group () is another expression, call the entire thing
+        // again
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        // call the visitor again to evaluate the inside expression
+        return expr.accept(this);
+    }
+
 }
