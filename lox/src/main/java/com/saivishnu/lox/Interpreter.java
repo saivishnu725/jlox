7e86b0152c26828c9fdb1e5d58fcd33bdb4a4e47
Sai Vishnu <saivishnu725@gmail.com>
2023-09-28 15:35:15 +0530

    Unary visitor

--- lox/src/main/java/com/saivishnu/lox/Interpreter.java
+++ lox/src/main/java/com/saivishnu/lox/Interpreter.java
@@ -20,4 +20,25 @@ class Interpreter implements Expr.Visitor<Object> {
         return expr.accept(this);
     }
 
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+        switch (expr.operator.type) {
+            case BANG:
+                // if the right is truthy, return false, else return true
+                return !isTruthy(right);
+            case MINUS:
+                // negate the value by converting it to a double
+                return -(double) right;
+        }
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
+        return true;
+    }
 }
