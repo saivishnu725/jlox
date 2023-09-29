e9e06be507fa9ee75ec1143663c62d51d3dbe26b
Sai Vishnu <saivishnu725@gmail.com>
2023-09-29 08:40:28 +0530

    formatting

--- lox/src/main/java/com/saivishnu/lox/Stmt.java
+++ lox/src/main/java/com/saivishnu/lox/Stmt.java
@@ -5,10 +5,12 @@ import java.util.List;
 abstract class Stmt {
 	interface Visitor<R> {
 		R visitExpressionStmt(Expression stmt);
+
 		R visitPrintStmt(Print stmt);
 	}
+
 	static class Expression extends Stmt {
-		Expression(Expr expression){
+		Expression(Expr expression) {
 			this.expression = expression;
 		}
 
@@ -16,10 +18,12 @@ abstract class Stmt {
 		<R> R accept(Visitor<R> visitor) {
 			return visitor.visitExpressionStmt(this);
 		}
+
 		final Expr expression;
 	}
+
 	static class Print extends Stmt {
-		Print(Expr expression){
+		Print(Expr expression) {
 			this.expression = expression;
 		}
 
@@ -27,6 +31,7 @@ abstract class Stmt {
 		<R> R accept(Visitor<R> visitor) {
 			return visitor.visitPrintStmt(this);
 		}
+
 		final Expr expression;
 	}
 
